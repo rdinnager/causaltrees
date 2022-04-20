a92ec6ffb6156743d07688b2d9c40ab64d668ad2
Russell Dinnage <rdinnage@fiu.edu>
2022-04-20 10:10:46 -0400

    doing some preliminary checking

--- R/make_data.R
+++ R/make_data.R
@@ -1,3 +1,14 @@
 ct_conditioner <- function(phy, form = NULL, data = NULL, framework = c("lme4", "mgcv", "brms", "inla")) {
-      
+  tips <- seq_along(phy$tip.label)
+  n_tip <- length(tips)
+  pairs <- fastmap::faststack()
+  while(length(tips) > 0) {
+    sisters <- phytools::getSisters(phy, tips[1])
+    sisters <- sisters[sisters <= n_tip]
+    if(length(sisters) > 0) {
+      sisters <- c(tips[1], sisters)
+      pairs$push(list(sisters))
+      tips <- tips[-sisters]
+    }
+  }
 }
